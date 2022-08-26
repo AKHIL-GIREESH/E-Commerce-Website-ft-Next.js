7af36ca37b14c691869dfae44d2f35bf4eb884e4
AKHIL-GIREESH <94656684+AKHIL-GIREESH@users.noreply.github.com>
2022-08-27 02:08:53 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -3,10 +3,10 @@ export default function Footer(){
         <footer className="Footer">
             <p className="Contact">Contact</p>
             <div className="Links">
-            <p><a target="_blank" href="https://github.com/AKHIL-GIREESH" className="Link">GitHub</a></p>
-            <p><a target="_blank" href="https://twitter.com/_0x416B68696C_" className="Link">Twitter</a></p>
-            <p><a target="_blank" href="https://www.linkedin.com/in/akhil-gireesh-3b6aa5227/" className="Link">LinkedIn</a></p>
-            <p><a target="_blank" href="https://solo.to/akhil_gireesh" className="Link">Website</a></p>
+            <p><a href="https://github.com/AKHIL-GIREESH" className="Link">GitHub</a></p>
+            <p><a href="https://twitter.com/_0x416B68696C_" className="Link">Twitter</a></p>
+            <p><a href="https://www.linkedin.com/in/akhil-gireesh-3b6aa5227/" className="Link">LinkedIn</a></p>
+            <p><a href="https://solo.to/akhil_gireesh" className="Link">Website</a></p>
             </div>
         </footer>
 )}
